c541892cf747ac26e524835eae4db977dc01449f
stephanie <bfssi.stephanie.diaz@gmail.com>
2025-02-11 08:26:54 -0500

    Project set up

---
+++ chhad_mailtrack/chhad_mailtrack/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from chhad_mailtrack.users.models import User
+from chhad_mailtrack.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
